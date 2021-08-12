41880630c54919b15ea7bf1f6f5636c065a77bca
Asawari2525 <84971974+Asawari2525@users.noreply.github.com>
2021-08-12 16:52:13 +0530

    Checkpoint 11
    Calculated Total price added with the help of js

--- scripts/detail.js
+++ scripts/detail.js
@@ -1,9 +1,20 @@
+var bookingFromDate;
+var bookingToDate;
 BookingForm();
 function BookingForm(){
-    var adultNumber = document.getElementById('adult');
-    var bookingFromDate = document.getElementById('fromDate');
-    var bookingToDate = document.getElementById('toDate');
+    var adultNumber = document.getElementById('adult').value;
+    var bookingFromDate = new Date(document.getElementById('fromDate').value);
+    var bookingToDate = new Date(document.getElementById('toDate').value);
     var afterBookingCalculatedPrice = document.getElementById('price');
-    var bookingNumberOfDays = (bookingToDate.value - bookingFromDate.value);
-    afterBookingCalculatedPrice.innerText += "Rs" + (adultNumber.value * bookingNumberOfDays * 1000);
+    if(bookingToDate>bookingFromDate && adultNumber>0){
+        afterBookingCalculatedPrice.value = "Rs. "+ payment();
+    }else{
+    afterBookingCalculatedPrice.innerText = "Rs. 0";
+}
+}
+function payment(){
+    var adultNumber = document.getElementById('adult').value;
+    var bookingFromDate = new Date(document.getElementById('fromDate').value);
+    var bookingToDate = new Date(document.getElementById('toDate').value);
+    return parseInt((bookingToDate - bookingFromDate) / (24 * 3600 * 1000 ))*adultNumber*1000;
 }
